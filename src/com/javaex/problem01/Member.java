1e3fa24a6df74f0af17b903499de1dbabf002a4a
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 16:31:03 +0900

    ex1

--- src/com/javaex/problem01/Member.java
+++ src/com/javaex/problem01/Member.java
@@ -2,26 +2,26 @@ package com.javaex.problem01;
 
 public class Member {
 	private int id ;
-	private Sring name ;
+	private String name ;
 	private int point ; 
 
-	public void SetName(String name) {
+	public void setname(String name) {
 		this.name=name;
 	}
-	public void SetId(int id) {
+	public void setid(int id) {
 		this.id=id;
 	}
-	public void SetPoint(int point) {
+	public void setpoint(int point) {
 		this.point=point;
 	}
 	
-	public void getName(String name) {
-		return name;
+	public String getname(){
+		 return name;
 	}
-	public void getId(int id) {
-		return name;
+	public int getid() {
+		return id;
 	}
-	public void getPoint(int point) {
-		return name;
+	public int getpoint() {
+		return point;
 	}
 }
