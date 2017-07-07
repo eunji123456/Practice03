fbe5dc3aaca91936f60e204e38accf8fa6c51a6c
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 17:00:14 +0900

    5번

--- src/com/javaex/problem05/StringUtil.java
+++ src/com/javaex/problem05/StringUtil.java
@@ -1,11 +1,14 @@
 package com.javaex.problem05;
 
 public class StringUtil {
-    
-    public static String concatString(){
-       
-        //메소드 내용작성
-        
-    }
+   
+	public static String concatString(String[] strArray) {
+		String resultStr ="";
+		// TODO Auto-generated method stub
+		for (int i=0;i<strArray.length;i++) {
+			 resultStr += strArray[i];
+		}
+		return resultStr;
+	}
 
 }
