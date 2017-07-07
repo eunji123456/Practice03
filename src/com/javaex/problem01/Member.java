b18f9ddbfced693d760d59d8a644706606b215a3
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 16:20:20 +0900

    1번문제

--- src/com/javaex/problem01/Member.java
+++ src/com/javaex/problem01/Member.java
@@ -1,5 +1,27 @@
 package com.javaex.problem01;
 
 public class Member {
+	private int id ;
+	private Sring name ;
+	private int point ; 
 
+	public void SetName(String name) {
+		this.name=name;
+	}
+	public void SetId(int id) {
+		this.id=id;
+	}
+	public void SetPoint(int point) {
+		this.point=point;
+	}
+	
+	public void getName(String name) {
+		return name;
+	}
+	public void getId(int id) {
+		return name;
+	}
+	public void getPoint(int point) {
+		return name;
+	}
 }
