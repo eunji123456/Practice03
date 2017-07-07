8ad0f8916cf56813919eb5113f416249a4955db7
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 17:30:27 +0900

    ex 7

--- src/com/javaex/problem07/Account.java
+++ src/com/javaex/problem07/Account.java
@@ -2,8 +2,32 @@ package com.javaex.problem07;
 
 public class Account {
 
-    private String accountNo;
-    private int balance;
+    private String accountNo;//계좌 
+    private int balance;//잔고 
+   
+
+	public Account(String accountNo) {
+		this.accountNo=accountNo;
+		System.out.println(accountNo+" 계좌가 개설 되었습니다");
+	}
+
+
+	public void deposit(int money) {
+		balance=balance+money;
+		
+	}
+
+
+	public void withdraw(int money) {
+		balance=balance-money;
+		
+	}
+
+
+	public void showBalance() {
+		System.out.println(" "+balance);
+		
+	}
     
     //생성자 작성
     
